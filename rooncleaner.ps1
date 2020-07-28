7bedc9ce6bbfa98d25812554f4d354a772a15472
Daniel Lundh <dlundh@gmail.com>
2020-07-28 10:35:54 +0200

    Update rooncleaner.ps1

--- rooncleaner.ps1
+++ rooncleaner.ps1
@@ -1,15 +1,16 @@
-# Reads Roon Excelfile, erases files in Roon library as needed
+# Reads Roon Excelfile, erases image files that can't be read by Roon
 # Author Daniel Lundh (c) 2020
 # Erases files with Reason: unable_to_load_image
+# YES, THIS WILL DELETE FILES. USE WITH CAUTION
+# usage rooncleaner.ps1 -infile filename
+# example:
+# rooncleaner.ps1 -infile C:\Users\dlh\Desktop\Skipped.xls
 
-#$FilePathXML = "c:\Users\dlh\Skipped.xls"
-#$FilePathCSV = "c:\Users\dlh\Skipped.csv"
-#$Excel = New-Object -ComObject Excel.Application
-#$Workbook = $Excel.Workbooks.Open($FilePathXML)
-#$Workbook.SaveAs($FilePathCSV, 6)
-#$Excel.Quit()
+param (
+    [Parameter(Mandatory=$true)][string]$infile
+)
 
-[xml]$ExcelXML = Get-Content -Path .\Skipped.xls
+[xml]$ExcelXML = Get-Content -Path $infile
 
 foreach ($Radnummer in (1..100)) {
     if ($ExcelXML.Workbook.Worksheet.Table.Row[$Radnummer].cell.data | Where-Object -f {$_.'#text' -eq "unable_to_load_image"}) {
